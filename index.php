bde84e9621736c9d136b0ac1ba5d76272f445432
LopezChristineAnne <lopezchristine019@gmail.com>
2022-04-14 19:19:16 +0800

    title1

--- index.php
+++ index.php
@@ -14,15 +14,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php">Music Spotlight</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
                     </ul>
                 </div>
             </div>
@@ -30,8 +30,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to Music Spotlight</h1>
+                <p class="lead">Where words fail, music speaks.</p>
                 
             </div>
         </header>
@@ -40,9 +40,99 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+
+                        <?php
+
+                        echo "<h2 class='text-primary'>Incomplete</h2>";
+                        echo "<p class='lead'>Sisqo</p>";
+
+                        echo "<p> Whoo</p>";
+                        echo "<p> Ooh</p>";
+                        echo "<p> Oh yeah yeah</p>";
+                        echo "<p> Listen</p>";
+                        echo "<br/>";
+
+                        echo "<p> Bright lights, fancy restaurants</p>";
+                        echo "<p> Everything in this world that a man could want</p>";
+                        echo "<p> Got a bank account bigger than the law should allow</p>";
+                        echo "<p> Still I'm lonely now</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pretty faces from the covers of the magazines</p>";
+                        echo "<p> From their covers to my covers wanna lay with me</p>";
+                        echo "<p> Fame and fortune still can't find</p>";
+                        echo "<p> Just a grown man runnin' out of time</p>";
+                        echo "<br/>";
+
+                        echo "<p> Even though it seems I have everything</p>";
+                        echo "<p> I don't wanna be a lonely fool</p>";
+                        echo "<p> All of the women, all the expensive cars</p>";
+                        echo "<p> All of the money don't amount to you</p>";
+                        echo "<p> So I can make believe I have everything</p>";
+                        echo "<p> But I can't pretend that I don't see</p>";
+                        echo "<p> That without you girl my life is incomplete</p>";
+                        echo "<p> Said without you girl ahh</p>";
+                        echo "<p> Listen</p>";
+                        echo "<br/>";
+
+                        echo "<p> Your perfume, your sexy lingerie</p>";
+                        echo "<p> Girl I remember it just like it was on yesterday</p>";
+                        echo "<p> A Thursday you told me you had fallen in love</p>";
+                        echo "<p> I wasn't sure that I was</p>";                      
+                        echo "<br/>";
+
+                        echo "<p> It's been a year, winter, summer, spring and fall</p>";
+                        echo "<p> But bein' without you just ain't livin' ain't nothing at all</p>";
+                        echo "<p> If I could travel back in time</p>";
+                        echo "<p> I'd relive the days you were mine</p>";
+                        echo "<br/>";
+
+                        echo "<p> Even though it seems I have everything</p>";
+                        echo "<p> I don't wanna be a lonely fool</p>";
+                        echo "<p> All of the women, all the expensive cars</p>";
+                        echo "<p> All of the money don't amount to you</p>";
+                        echo "<p> So I can make believe I have everything</p>";
+                        echo "<p> But I can't pretend that I don't see</p>";
+                        echo "<p> That without you girl my life is incomplete</p>";
+                        echo "<p> Oh yeah that without you girl ahh</p>";
+                        echo "<br/>";
+
+                        echo "<p> I just can't help lovin' you</p>";
+                        echo "<p> But I loved you much too late</p>";
+                        echo "<p> I'd give anything</p>";
+                        echo "<p> And everything</p>";
+                        echo "<p> To hear you say</p>";
+                        echo "<p> That you'll stay</p>";
+                        echo "<br/>";
+
+                        echo "<p> Even though it seems I have everything</p>";
+                        echo "<p> I don't wanna be a lonely fool</p>";
+                        echo "<p> All of the women, all the expensive cars</p>";
+                        echo "<p> All of the money don't amount to you</p>";
+                        echo "<p> So I can make believe I have everything</p>";
+                        echo "<p> But I can't pretend that I don't see</p>";
+                        echo "<p> That without you girl my life is incomplete</p>";
+                        echo "<br/>";
+
+                        echo "<p> Even though it seems I have everything</p>";
+                        echo "<p> I don't wanna be a lonely fool</p>";
+                        echo "<p> All of the women, all the expensive cars</p>";
+                        echo "<p> All of the money don't amount to you</p>";
+                        echo "<p> So I can make believe I have everything</p>";
+                        echo "<p> But I can't pretend that I don't see</p>";
+                        echo "<p> That without you girl my life is incomplete</p>";
+                        echo "<br/>";
+
+                        echo "<p> Without you girl</p>";
+                        echo "<p> Without you girl my life</p>";
+                        echo "<p> Without you girl my life, my life is incomplete</p>";
+                        echo "<p> Without you girl my life is incomplete</p>";
+                        echo "<p> Oh yeah, my life is incomplete</p>";
+                        echo "<p> Oh yeah, yeah, yeah</p>";
+                        echo "<br/>";
+                        
+                        ?>
+
                     </div>
                 </div>
             </div>
