9c6df6fbe6a01dab86ffcda0a2e3c92402a678c0
LopezChristineAnne <lopezchristine019@gmail.com>
2022-04-14 20:17:24 +0800

    title5

---
+++ Song4.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php">Music Spotlight</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Spotlight</h1>
+                <p class="lead">Where words fail, music speaks.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+
+                        echo "<h2 class='text-primary'>Weak</h2>";
+                        echo "<p class='lead'>SWV</p>";
+
+                        echo "<p> I don't know what it is that you've done to me</p>";
+                        echo "<p> But it's caused me to act in such a crazy way</p>";
+                        echo "<p> Whatever it is that you do when you do what you're doing</p>";
+                        echo "<p> It's a feeling that I want to stay</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause my heart starts beating triple time</p>";
+                        echo "<p> With thoughts of loving you on my mind</p>";
+                        echo "<p> I can't figure out just what to do</p>";
+                        echo "<p> When the cause and cure is you</p>";
+                        echo "<br/>";
+
+                        echo "<p> I get so weak in the knees I can hardly speak</p>";
+                        echo "<p> I lose all control and something takes over me</p>";
+                        echo "<p> In a daze, your love's so amazing</p>";
+                        echo "<p> It's not a phase</p>";
+                        echo "<p> I want you to stay with me by my side</p>";
+                        echo "<p> I swallow my pride,</p>";
+                        echo "<p> Your love is so sweet</p>";
+                        echo "<p> It knocks me right off of my feet</p>";
+                        echo "<p> Can't explain why your loving makes me weak</p>";
+                        echo "<br/>";
+
+                        echo "<p> Time after time, after time I try to fight it</p>";
+                        echo "<p> But your love is strong it keeps on holding on</p>";
+                        echo "<p> Resistance is down when you're around, cries fading</p>";
+                        echo "<p> In my condition I don't want to be alone</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause my heart starts beating triple time</p>";
+                        echo "<p> With thoughts of loving you on my mind</p>";
+                        echo "<p> I can't figure out just what to do</p>";
+                        echo "<p> When the cause and cure is you</p>";
+                        echo "<br/>";
+
+                        echo "<p> I get so weak in the knees I can hardly speak</p>";
+                        echo "<p> I lose all control and something takes over me</p>";
+                        echo "<p> In a daze, your love's so amazing</p>";
+                        echo "<p> It's not a phase</p>";
+                        echo "<p> I want you to stay with me by my side</p>";
+                        echo "<p> I swallow my pride,</p>";
+                        echo "<p> Your love is so sweet</p>";
+                        echo "<p> It knocks me right off of my feet</p>";
+                        echo "<p> Can't explain why your loving makes me weak</p>";
+                        echo "<br/>";
+
+                        echo "<p> I try hard to fight it</p>";
+                        echo "<p> No way can I deny it</p>";
+                        echo "<p> Your love's so sweet</p>";
+                        echo "<p> It knocks me off my feet</p>";
+                        echo "<br/>";
+
+                        echo "<p> I get so weak in the knees I can hardly speak</p>";
+                        echo "<p> I lose all control and something takes over me</p>";
+                        echo "<p> In a daze, your love's so amazing</p>";
+                        echo "<p> It's not a phase</p>";
+                        echo "<p> I want you to stay with me by my side</p>";
+                        echo "<p> I swallow my pride,</p>";
+                        echo "<p> Your love is so sweet</p>";
+                        echo "<p> It knocks me right off of my feet</p>";
+                        echo "<p> Can't explain why your loving makes me weak</p>";
+                        echo "<br/>";
+
+                        echo "<p> I get so weak in the knees I can hardly speak</p>";
+                        echo "<p> I lose all control and something takes over me</p>";
+                        echo "<p> In a daze, your love's so amazing</p>";
+                        echo "<p> It's not a phase</p>";
+                        echo "<p> I want you to stay with me by my side</p>";
+                        echo "<p> I swallow my pride,</p>";
+                        echo "<p> Your love is so sweet</p>";
+                        echo "<p> It knocks me right off of my feet</p>";
+                        echo "<p> Can't explain why your loving makes me weak</p>";
+                        echo "<br/>";
+
+                        
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Anne Lopez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
