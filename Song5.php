ce32de60f9d5beeaba13f0b5ea85ca744deeb879
LopezChristineAnne <lopezchristine019@gmail.com>
2022-04-14 20:30:49 +0800

    title6

---
+++ Song5.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php">Music Spotlight</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Spotlight</h1>
+                <p class="lead">Where words fail, music speaks.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+
+                        echo "<h2 class='text-primary'>Hold On</h2>";
+                        echo "<p class='lead'>Chord Overstreet</p>";
+
+                        echo "<p> Loving and fighting</p>";
+                        echo "<p> Accusing, denying</p>";
+                        echo "<p> I can't imagine a world with you gone</p>";
+                        echo "<p> The joy and the chaos, the demons we're made of</p>";
+                        echo "<p> I'd be so lost if you left me alone...</p>";
+                        echo "<p> You locked yourself in the bathroom;</p>";
+                        echo "<p> I can't imagine a world with you gone</p>";
+                        echo "<p> Lying on the floor when I break through</p>";
+                        echo "<p> I pull you in to feel your heartbeat</p>";
+                        echo "<p> Can you hear me screaming Please don't leave me!</p>";
+                        echo "<br/>";
+
+                        echo "<p> Hold on, I still want you</p>";
+                        echo "<p> Come back, I still need you</p>";
+                        echo "<p> Let me take your hand, I'll make it right</p>";
+                        echo "<p> I swear to love you all my life</p>";
+                        echo "<p> Hold on, I still need you</p>";
+                        echo "<br/>";
+
+                        echo "<p> A long endless highway, you're silent beside me</p>";
+                        echo "<p> Driving a nightmare I can't escape from</p>";
+                        echo "<p> Helplessly praying, the light isn't fading</p>";
+                        echo "<p> Hiding the shock and the chill in my bones</p>";
+                        echo "<p> They took you away on a table</p>";
+                        echo "<p> I pace back and forth as you lay still</p>";
+                        echo "<p> They pull you in to feel your heartbeat...</p>";
+                        echo "<p> Can you hear me screaming, Please don't leave me!</p>";
+                        echo "<br/>";
+
+                        echo "<p> Hold on, I still want you</p>";
+                        echo "<p> Come back, I still need you</p>";
+                        echo "<p> Let me take your hand, I'll make it right</p>";
+                        echo "<p> I swear to love you all my life</p>";
+                        echo "<p> Hold on, I still need you</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause my heart starts beating triple time</p>";
+                        echo "<p> With thoughts of loving you on my mind</p>";
+                        echo "<p> I can't figure out just what to do</p>";
+                        echo "<p> When the cause and cure is you</p>";
+                        echo "<br/>";
+
+                        echo "<p> I don't wanna let go</p>";
+                        echo "<p> I know I'm not that strong</p>";
+                        echo "<p> I just wanna hear you</p>";
+                        echo "<p> Saying, Baby, let's go home.</p>";
+                        echo "<p> Let's go home</p>";
+                        echo "<p> Yeah, I just wanna take you home</p>";
+                        echo "<br/>";
+
+                        echo "<p> Hold on, I still want you</p>";
+                        echo "<p> Come back, I still need you</p>";
+                        echo "<br/>";
+
+                        
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Anne Lopez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
