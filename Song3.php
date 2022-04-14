d04baa68d87107730af733c639f6478f907759e3
LopezChristineAnne <lopezchristine019@gmail.com>
2022-04-14 20:01:42 +0800

    title4

---
+++ Song3.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="https://localhost/melomaniac-activity/index.php">Music Spotlight</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="https://localhost/melomaniac-activity/Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Music Spotlight</h1>
+                <p class="lead">Where words fail, music speaks.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+
+                        echo "<h2 class='text-primary'>The Sequel</h2>";
+                        echo "<p class='lead'>St. Vincent</p>";
+
+                        echo "<p> Oh honey</p>";
+                        echo "<p> I was there in the dark where you lay</p>";
+                        echo "<p> And I saw you</p>";
+                        echo "<p> With a scent on your hands</p>";
+                        echo "<p> Going out to get you something</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh honey</p>";
+                        echo "<p> I was there in the dark where you lay</p>";
+                        echo "<p> And I saw you</p>";
+                        echo "<p> With a scent on your hands</p>";
+                        echo "<p> Going out to get you some light</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> One, two, three flight</p>";
+                        echo "<p> Apartment streetside</p>";
+                        echo "<p> Bodies like wrecking balls</p>";
+                        echo "<p> Fuck, fuck with dynamite</p>";
+                        echo "<br/>";
+
+                        
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Anne Lopez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
